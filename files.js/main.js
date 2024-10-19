538726f9ffc88d17ce0f36997ff2a94e493cfc58
Gennadiy99 <login841@gmail.com>
2024-10-19 13:37:45 +0200

    add in JS click (li)

---
+++ files.js/main.js
@@ -0,0 +1,21 @@
+let link_services = document.getElementById('services');
+let link_about = document.getElementById('about_Us');
+let link_cases = document.getElementById('cases');
+
+const modWind_1 = document.getElementById('window_services');
+const modWind_2 = document.getElementById('window_about');
+const modWind_3 = document.getElementById('window_cases');
+
+link_services.addEventListener('click', function(){
+  modWind_1.classList.toggle('mod__open');
+  link_services.classList.toggle('color-active');
+})
+link_about.addEventListener('click', function(){
+  modWind_2.classList.toggle('mod__open');
+  link_about.classList.toggle('color-active');
+})
+
+link_cases.addEventListener('click', function(){
+  modWind_3.classList.toggle('mod__open');
+  link_cases.classList.toggle('color-active');
+})
