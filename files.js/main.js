3fbf15c906a00ab88dcd4bcaafaaf593b8d1e78f
Gennadiy99 <login841@gmail.com>
2024-10-23 23:37:49 +0200

    add open side-menu + close

--- files.js/main.js
+++ files.js/main.js
@@ -19,3 +19,15 @@ link_cases.addEventListener('click', function(){
   modWind_3.classList.toggle('mod__open');
   link_cases.classList.toggle('color-active');
 })
+// открытие бокового меню. (РАБОТАЕТпока только на центральной странице)
+const navBar = document.querySelector('.nav-bar-menu'); 
+let sideBar = document.querySelector('.side-menu');
+navBar.addEventListener('click', function(){
+  console.log('Click');
+  sideBar.classList.toggle('block-show');
+})
+// закрытие бокового меню.
+const closeSideMenu = document.querySelector('.side-menu-beck');
+closeSideMenu.addEventListener('click', function(){
+  sideBar.classList.toggle('block-show');
+})
