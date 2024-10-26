4f8e7b7483c7e8b5a5f9ef1b7cc747be54fb9066
Gennadiy99 <login841@gmail.com>
2024-10-26 23:10:45 +0200

    add close mod-win icon

--- files.js/main.js
+++ files.js/main.js
@@ -6,6 +6,7 @@ const modWind_1 = document.getElementById('window_services');
 const modWind_2 = document.getElementById('window_about');
 const modWind_3 = document.getElementById('window_cases');
 
+// открытие/закрытие модальных окон кликом на СПИСОК + установка color-active
 link_services.addEventListener('click', function(){
   console.log('Клик по Списку');
   modWind_1.classList.toggle('mod__open');
@@ -20,6 +21,27 @@ link_cases.addEventListener('click', function(){
   modWind_3.classList.toggle('mod__open');
   link_cases.classList.toggle('color-active');
 })
+// Закрытие Мод окна СЕРВИС через иконку "крестик"
+const closeServ = document.getElementById('services-close');
+closeServ.addEventListener('click', function (){
+  modWind_1.classList.toggle('mod__open');
+  link_services.classList.toggle('color-active');
+})
+
+// Закрытие Мод окна О НАС через иконку "крестик"
+const closeAbout = document.getElementById('close-about');
+closeAbout.addEventListener('click', function (){
+  modWind_2.classList.toggle('mod__open');
+  link_about.classList.toggle('color-active');
+})
+
+// Закрытие Мод окна О НАС через иконку "крестик"
+const closeCases = document.getElementById('close-cases');
+closeCases.addEventListener('click', function (){
+  modWind_3.classList.toggle('mod__open');
+  link_cases.classList.toggle('color-active');
+})
+
 // открытие бокового меню. (РАБОТАЕТпока только на центральной странице)
 const navBar = document.querySelector('.nav-bar-menu'); 
 let sideBar = document.querySelector('.side-menu');
